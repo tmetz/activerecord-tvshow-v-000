9974593488ccd04f71b0404c0d535e203c4c0240
Tammy Metz <tammy@tammymetz.com>
2018-10-03 20:44:51 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,2 +1,15 @@
 class Show < ActiveRecord::Base
+
+  def highest_rating
+    Show.maximum
+  end
+
+  def most_popular_show
+  end
+
+  def lowest_rating
+  end
+
+  def least_popular_show
+  end
 end
